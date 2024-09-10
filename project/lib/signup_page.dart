0246a9546a911c4a94ef88d4406b4919ee7118dc
Kshitij Sakhuja <118502165+kshitijsakhuja@users.noreply.github.com>
2024-09-10 19:48:09 +0530

    .

--- project/lib/signup_page.dart
+++ project/lib/signup_page.dart
@@ -15,13 +15,13 @@ class SignUpScreen extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.green,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Login / Sign Up Tabs
-                Row(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Login / Sign Up Tabs
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     GestureDetector(
@@ -38,7 +38,6 @@ class SignUpScreen extends StatelessWidget {
                         "Log In",
                         style: TextStyle(
                           color: Colors.white,
-                          //decoration: TextDecoration.underline,
                         ),
                       ),
                     ),
@@ -52,99 +51,105 @@ class SignUpScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                const SizedBox(height: 16),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(40),
-                      ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: Container(
+                  width: double.infinity, // Full width
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(40),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 16),
-                          const Text(
-                            'Sign up with your email and phone number',
-                            style: TextStyle(fontSize: 16),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Sign up with your email and phone number',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(height: 20),
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            border: UnderlineInputBorder(),
                           ),
-                          const SizedBox(height: 20),
-                          const TextField(
-                            decoration: InputDecoration(
-                              labelText: "Email",
-                              border: UnderlineInputBorder(),
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          children: [
+                            // Country Code Dropdown
+                            Expanded(
+                              flex: 2,
+                              child: DropdownButtonFormField<String>(
+                                decoration: const InputDecoration(
+                                  border: UnderlineInputBorder(),
+                                ),
+                                value: '+91',
+                                items: <String>['+91', '+1', '+44', '+61']
+                                    .map<DropdownMenuItem<String>>(
+                                        (String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(),
+                                onChanged: (String? newValue) {},
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 20),
-                          Row(
-                            children: [
-                              // Country Code Dropdown
-                              Expanded(
-                                flex: 2,
-                                child: DropdownButtonFormField<String>(
-                                  decoration: const InputDecoration(
-                                    border: UnderlineInputBorder(),
-                                  ),
-                                  value: '+91',
-                                  items: <String>['+91', '+1', '+44', '+61']
-                                      .map<DropdownMenuItem<String>>(
-                                          (String value) {
-                                        return DropdownMenuItem<String>(
-                                          value: value,
-                                          child: Text(value),
-                                        );
-                                      }).toList(),
-                                  onChanged: (String? newValue) {},
-                                ),
-                              ),
-                              const SizedBox(width: 10),
-                              // Phone Number Field
-                              const Expanded(
-                                flex: 5,
-                                child: TextField(
-                                  keyboardType: TextInputType.phone,
-                                  decoration: InputDecoration(
-                                    hintText: "Phone number",
-                                    border: UnderlineInputBorder(),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 40),
-                          Center(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 16.0, horizontal: 40.0),
-                                backgroundColor: Colors.black,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                              ),
-                              onPressed: () {
-                                // Handle sign up
-                              },
-                              child: const Text(
-                                'SIGN UP',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  letterSpacing: 1.5,
+                            const SizedBox(width: 10),
+                            // Phone Number Field
+                            const Expanded(
+                              flex: 5,
+                              child: TextField(
+                                keyboardType: TextInputType.phone,
+                                decoration: InputDecoration(
+                                  hintText: "Phone number",
+                                  border: UnderlineInputBorder(),
                                 ),
                               ),
                             ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+
+                        // Spacer pushes the button to the bottom
+                        const Spacer(),
+
+                        Center(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 40.0),
+                              backgroundColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                            onPressed: () {
+                              // Handle sign up
+                            },
+                            child: const Text(
+                              'SIGN UP',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                letterSpacing: 1.5,
+                              ),
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(height: 20), // Spacing under the button
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
